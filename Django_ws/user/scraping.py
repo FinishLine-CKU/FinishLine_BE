8039ecdf3db438f7c3a6c238cc4a9ade5b451173
gnawH <sun30126331@gmail.com>
2025-01-08 06:15:01 +0900

    feat 예외처리 추가

--- Django_ws/user/scraping.py
+++ Django_ws/user/scraping.py
@@ -1,35 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import UnexpectedAlertPresentException
 from bs4 import BeautifulSoup
 
 def scraping(studentId, studentPW):
-    options = webdriver.ChromeOptions()
-    
-    options.add_argument('headless')
-    options.add_argument('--disable-gpu')
-    options.add_experimental_option("detach", True)
-    options.add_experimental_option('excludeSwitches', ['disable-popup-blocking'])
+    try:
+        options = webdriver.ChromeOptions()
+        
+        options.add_argument('headless')
+        options.add_argument('--disable-gpu')
+        options.add_experimental_option("detach", True)
+        options.add_experimental_option('excludeSwitches', ['disable-popup-blocking'])
 
-    driver = webdriver.Chrome(options = options)
-    driver.get('https://info.cku.ac.kr/haksa/common/loginForm2.jsp')
-    driver.implicitly_wait(1)
-    driver.find_element(By.XPATH, '//area[@title="통합로그인"]').click()
-    driver.implicitly_wait(1)
+        driver = webdriver.Chrome(options = options)
+        driver.get('https://info.cku.ac.kr/haksa/common/loginForm2.jsp')
+        driver.implicitly_wait(1)
+        driver.find_element(By.XPATH, '//area[@title="통합로그인"]').click()
+        driver.implicitly_wait(1)
 
-    ckuID = driver.find_element(By.ID, 'login_id')
-    ckuID.send_keys(studentId)
-    ckuPW = driver.find_element(By.ID, 'login_pwd')
-    ckuPW.send_keys(studentPW)
-    driver.find_element(By.XPATH, '//a[@class="login"]').click()
-    driver.implicitly_wait(1)
+        ckuID = driver.find_element(By.ID, 'login_id')
+        ckuID.send_keys(studentId)
+        ckuPW = driver.find_element(By.ID, 'login_pwd')
+        ckuPW.send_keys(studentPW)
+        driver.find_element(By.XPATH, '//a[@class="login"]').click()
+        driver.implicitly_wait(1)
 
-    driver.find_element(By.XPATH, '//a[@class="snbmenu1"]').click()
-    driver.find_element(By.XPATH, '//a[text()="학생신상기록카드조회"]').click()
+        driver.find_element(By.XPATH, '//a[@class="snbmenu1"]').click()
+        driver.find_element(By.XPATH, '//a[text()="학생신상기록카드조회"]').click()
 
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    student_id = soup.find('td', class_='num').text
-    name = soup.find('th', class_='pad_3').find_previous_sibling('td').text
-    major, grade = soup.find('th', string='학과/학년').find_next('td').text.split()
+        student_id = soup.find('td', class_='num').text
+        name = soup.find('th', class_='pad_3').find_previous_sibling('td').text
+        major, grade = soup.find('th', string='학과/학년').find_next('td').text.split()
 
-    return student_id, name, major
+        return student_id, name, major
+    except UnexpectedAlertPresentException:
+        return '아이디 또는 비밀번호가 올바르지 않습니다.'
+    except:
+        return '시스템 오류 : 관리자에게 문의 부탁드립니다.'
--- Django_ws/user/views.py
+++ Django_ws/user/views.py
@@ -5,14 +5,15 @@ from rest_framework.response import Response
 @api_view(['POST'])
 def student_auth(request):
     if request.method == 'POST':
-        data = request.data
-        studentId = data.get('studentId')
-        studentPW = data.get('studentPW')
-        student_id, name, major = scraping(studentId, studentPW)
-        print(student_id, name, major)
-
-    return Response ({
-        'student_id': student_id, 
-        'name' : name, 
-        'major' : major
-        })
+        try:
+            data = request.data
+            studentId = data.get('studentId')
+            studentPW = data.get('studentPW')
+            student_id, name, major = scraping(studentId, studentPW)
+            print(student_id, name, major)
+            data = {'student_id': student_id, 'name' : name, 'major' : major}
+        except:
+            fault = scraping(studentId, studentPW)
+            print(fault)
+            data = {'fault' : fault}
+    return Response (data)
