f5c6c782bc31f4adba63d7024ceaf0b481fd8d8a
gnawH <sun30126331@gmail.com>
2025-01-10 01:54:28 +0900

    feat 회원가입 로직 1차 개발 완료 (예외 처리 상세화 필요)

--- Django_ws/user/models.py
+++ Django_ws/user/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=20)
-    student_id = models.CharField(max_length=10, unique=True, primary_key=True)
-    password = models.CharField(max_length=20)
-    major = models.CharField(max_length=40)
-    sub_major_type = models.CharField(max_length=5, blank=True, null=True)
-    sub_major = models.CharField(max_length=40, blank=True, null=True)
-    micro_degree = models.CharField(max_length=30, blank=True, null=True)
+    name = models.CharField(max_length=50)
+    student_id = models.CharField(max_length=30, unique=True, primary_key=True)
+    password = models.CharField(max_length=100)
+    major = models.CharField(max_length=100)
+    sub_major_type = models.CharField(max_length=30, blank=True, null=True)
+    sub_major = models.CharField(max_length=100, blank=True, null=True)
+    micro_degree = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
--- Django_ws/user/views.py
+++ Django_ws/user/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework.decorators import api_view
 from .scraping import scraping
 from .models import User
@@ -5,30 +6,44 @@ from rest_framework.response import Response
 
 @api_view(['POST'])
 def student_auth(request):
-    if request.method == 'POST':
-        data = request.data
-        studentId = data.get('studentId')
-        studentPW = data.get('studentPW')
-        result = scraping(studentId, studentPW)
-        if isinstance(result, tuple):
-            student_id, name, major = result
-            data = {'student_id': student_id, 'name' : name, 'major' : major}
-        else:
-            error = result
-            data = {'error' : error}
-        print(data)
+    data = request.data
+    studentId = data.get('studentId')
+    studentPW = data.get('studentPW')
+    result = scraping(studentId, studentPW)
+    if isinstance(result, tuple):
+        student_id, name, major = result
+        data = {'student_id': student_id, 'name' : name, 'major' : major}
+    else:
+        error = result
+        data = {'error' : error}
+    print(data)
     return Response (data)
 
 @api_view(['POST'])
 def register_info(request):
-    if request.method == 'POST':
-        data = request.data
-        name = data.get('name')
-        major = data.get('major')
-        student_id = data.get('student_id')
-        additionalMajorType = data.get('additionalMajorType')
-        additionalMajor = data.get('additionalMajor')
-        microDegree = data.get('microDegree')
-        password = data.get('password')
-        print(data)
-    return Response (data)
+    data = request.data
+    name = data.get('name')
+    major = data.get('major')
+    student_id = data.get('student_id')
+    sub_major_type = data.get('additionalMajorType')
+    sub_major = data.get('additionalMajor')
+    micro_degree = data.get('microDegree')
+    password = data.get('password')
+    if name and major and student_id and password:
+        try:
+            user = User.objects.create(
+                name = name,
+                major = major,
+                student_id = student_id,
+                sub_major_type = sub_major_type,
+                sub_major = sub_major,
+                micro_degree = micro_degree,
+                password = make_password(password)
+            )
+            print(user.name, user.major, user.student_id, user.sub_major_type, user.sub_major, user.micro_degree, user.password)
+            return Response (True)
+        except Exception as e:
+            print(f'DB 저장 오류: {repr(e)}')
+            return Response (False)
+    print('회원가입 필수 데이터 누락')
+    return Response (False)
