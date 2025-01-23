d47fe84b5a9bd5d2bbe3bf37f23cbeffc1324f4a
gnawH <sun30126331@gmail.com>
2025-01-24 04:13:40 +0900

    feat 마이페이지 회원 정보 조회

--- Django_ws/user/views.py
+++ Django_ws/user/views.py
@@ -73,3 +73,24 @@ def check_register(request):
         data = {'error' : error}
     print(data)
     return Response (data)
+
+@api_view(['POST'])
+def my_info(request):
+    data = request.data
+    name = data.get('name')
+    if User.objects.filter(name = name).exists():
+        user = User.objects.filter(name = name).first()
+        data = {
+            'major' : user.major,
+            'student_id' : user.student_id,
+        }
+        if user.sub_major_type and user.sub_major:
+            data['sub_major_type'] = user.sub_major_type
+            data['sub_major'] = user.sub_major
+        if user.micro_degree:
+            data['micro_degree'] = user.micro_degree
+    else:
+        error = '해당 없음',
+        data = {'error' : error}
+    print(data)
+    return Response (data)
