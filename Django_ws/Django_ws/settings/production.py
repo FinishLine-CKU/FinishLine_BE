361d909b9ee43e9561b1ac6122e22da527f2e278
root <root@ip-172-31-5-223.ap-northeast-2.compute.internal>
2025-02-19 04:09:43 +0000

    fix cache 충돌해결

--- Django_ws/Django_ws/settings/production.py
+++ Django_ws/Django_ws/settings/production.py
@@ -1,13 +1,126 @@
-from .base import *
-import os
+# settings / production.py
 
-DEBUG = False
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-d0qm-4^*6##fao9=69hrqjrk*1^)bo90!&f%!*q^xaj(8@t804'
+
+
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    #추가 라이브러리 등록
+    'rest_framework',
+    'corsheaders',
+
+    #추가 app 등록
+    'user',
+    'graduation',
+    'manages',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'Django_ws.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+WSGI_APPLICATION = 'Django_ws.wsgi.application'
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = 'ko-kr'
+
+TIME_ZONE = 'Asia/Seoul'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = '/static/'
+
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'OPTIONS',
+    'PUT',
+    'DELETE',
+]
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'FinishLine_DB',
+        'USER': 'FinishLine',
+        'PASSWORD': 'finishline',
+        'HOST': 'finishline-mysql.chkyek42quy7.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
--- Django_ws/user/scraping.py
+++ Django_ws/user/scraping.py
@@ -9,6 +9,7 @@ def scraping(studentId, studentPW):
         
         options.add_argument('headless')
         options.add_argument('--disable-gpu')
+        options.add_argument("--no-sandbox")
         options.add_experimental_option("detach", True)
         options.add_experimental_option('excludeSwitches', ['disable-popup-blocking'])
 
