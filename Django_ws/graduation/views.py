d5d6ee7ee7f5dc4510ff3522c2bb05daec2e2020
gnawH <sun30126331@gmail.com>
2025-04-10 22:58:50 +0900

    rename 교양필수부족영역 needNessArea => lackEssentialGETopic 변경

--- Django_ws/graduation/views.py
+++ Django_ws/graduation/views.py
@@ -235,7 +235,7 @@ def general_check(request):
         'general_data': {
             'lackEssentialGE': result.get("교양필수 부족 학점", []), #
             'lackChoiceGE': result.get("교양선택 부족 학점", []), #
-            '교양필수_부족_영역': result.get("교양필수 부족 영역", []), 
+            'lackEssentialGETopic': result.get("교양필수 부족 영역", []), 
             '교양선택_부족_영역': result.get("교양선택 부족 영역", []), 
             'doneEssentialGE': result.get("교양필수 이수 학점", []), 
             'doneChoiceGE': result.get("교양선택 이수 학점", []), 
